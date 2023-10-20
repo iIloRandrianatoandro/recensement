7e7d33f42b10d961921a34140c931f4336f88158
unknown <iloorandrianatoandro.com>
2023-10-20 18:19:53 +0300

    import liste teste ok

--- app/Http/Controllers/RecensementController.php
+++ app/Http/Controllers/RecensementController.php
@@ -17,21 +17,22 @@ use DB;
 class RecensementController extends Controller
 {
     public function import(Request $req)
-    {
+    {   
         $excel = Excel::toArray(new RecensementsImport, $req->file);
         $annee=$req->annee;
         $premiereUtilisation=$req->premiereUtilisation;
-        $array=array_slice($excel[0], 3);
+        $nomenclature=$req->nomenclature;
+        $array=array_slice($excel[0], 3); // ne prend pas en compte les 3 premiere lignes du fichier excel cad titres
         foreach($array as $array){
             //$premiereUtilisation=false;
-            if($premiereUtilisation){ //si premiere utilisation
+            /*if($premiereUtilisation === "true"){ //si premiere utilisation
                // creer le materiel
                 $materiel=new materiel;
                 $materiel->designation=$array[0];
-                $materiel->nomenclature='3';
+                $materiel->nomenclature=$nomenclature;
                 $materiel->especeUnite=$array[7]; 
 
-                //verifier le materiel existe deja
+                //trouver le nombre de doublon
                 $nbMaterielTableau=DB::select("SELECT COUNT(idMateriel) FROM materiels WHERE designation = :designation", ['designation' => $array[0]]);
                 $nbMateriel=$nbMaterielTableau[0]->{'COUNT(idMateriel)'};
                 $materiel->doublon=$nbMateriel+1;
@@ -49,16 +50,18 @@ class RecensementController extends Controller
                 $recensement->materiel_id=$idMateriel;
                 $recensement->annee=$annee;
                 $recensement->save();
-            }
+            }*/
             // s'il y a une nouvelle entree de materiel, ajouter le materiel à la base de donnees 
-            elseif($array[3]!=null){ //s'il y a une nouvelle entree
+            if(($array[3]!=null) || ($premiereUtilisation==="true")){ //s'il y a une nouvelle entree 
                 //creer materiel
                 $materiel=new materiel;
                 $materiel->designation=$array[0];
-                $materiel->nomenclature='3';
+                $materiel->nomenclature=$nomenclature;
                 $materiel->especeUnite=$array[7];
+
                 $designation=$array[0];
 
+                //nombre de doublon du materiel
                 $nbDoublonTableau = DB::select("SELECT COUNT(idMateriel) FROM materiels WHERE designation = :designation", ['designation' => $designation]);
                 $nbDoublon=$nbDoublonTableau[0]->{'COUNT(idMateriel)'};
                 $materiel->doublon=$nbDoublon+1;
@@ -78,8 +81,8 @@ class RecensementController extends Controller
             else{ //ajout recensement sans nouvelle entree et non premiere utilisation
                 $designation=$array[0];
                 $listeDoublonTableau = DB::select("SELECT idMateriel FROM materiels WHERE designation = :designation", ['designation' => $designation]);
-                foreach($listeDoublonTableau as $a){
-                    $materiel_id=$a->{'idMateriel'};
+                foreach($listeDoublonTableau as $materiel){
+                    $materiel_id=$materiel->{'idMateriel'};
                     $nbRecensementTableau=DB::select("SELECT count(idRecensement) FROM recensements WHERE materiel_id = :materiel_id and annee= :annee", ['materiel_id' => $materiel_id,'annee'=>$annee]);
                     $nbRecensement= $nbRecensementTableau[0]->{"count(idRecensement)"};
                     if($nbRecensement==0){//recensement non existant sur le materiel
