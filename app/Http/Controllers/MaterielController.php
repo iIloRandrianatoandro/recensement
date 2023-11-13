93b363115f193cc5f8429a6cd71c427d36d36e37
unknown <iloorandrianatoandro.com>
2023-11-13 09:08:34 +0300

    modifier materiel ok

--- app/Http/Controllers/MaterielController.php
+++ app/Http/Controllers/MaterielController.php
@@ -10,8 +10,27 @@ use DB;
 class MaterielController extends Controller
 {
     public function listeMateriel(){  
+        $nomenclatures = DB::table('materiels')
+            ->select('nomenclature')
+            ->groupBy('nomenclature')
+            ->get();
         $listeMateriel=DB::select("select * from materiels;");
-        return $listeMateriel;
+        //liste materiel par nomenclature
+        $MaterielParNomenclature = [];
+        foreach ($nomenclatures as $nomenclature) {
+            $materiel = DB::table('materiels')
+            ->where('nomenclature', $nomenclature->nomenclature)
+            ->select(
+                'idMateriel',
+                'designation' ,
+                'especeUnite',
+                'nomenclature',
+            )
+            ->get();
+    
+            $MaterielParNomenclature[$nomenclature->nomenclature] = $materiel;
+        }
+        return ['nomenclatures'=>$nomenclatures,'MaterielParNomenclature'=>$MaterielParNomenclature,'listeMateriel'=>$listeMateriel];
     }
     public function rechercherMateriel($designation){
         $listeMaterielCorrespondant=DB::select("select * from materiels where designation like '%$designation%'; ");
